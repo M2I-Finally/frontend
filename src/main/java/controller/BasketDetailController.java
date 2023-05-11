4c0dd5a1514006b2909aaabefb74d0f34ff6a211
Melanie Hautekeur <mehautekeur@gmail.com>
2023-05-11 10:58:23 +0200

    BasketDetail Controller creation

--- src/main/java/controller/BasketDetailController.java
+++ src/main/java/controller/BasketDetailController.java
@@ -1,5 +1,23 @@
 package controller;
 
 public class BasketDetailController {
-
+	// Methode GET
+	public void getMapping() {
+		
+	}
+	
+	// Methode POST
+	public void postMapping() {
+		
+	}
+	
+	// Methode PUT
+	public void updateMapping() {
+		
+	}
+	
+	// Methode DELETE
+	public void deleteMapping() {
+		
+	}
 }
