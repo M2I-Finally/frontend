1968bd0a6382be3874be0dc91bcdd45c99812b39
Melanie Hautekeur <mehautekeur@gmail.com>
2023-05-11 11:49:52 +0200

    Adding basketDetails in Basket entity

--- src/main/java/model/entities/Basket.java
+++ src/main/java/model/entities/Basket.java
@@ -1,28 +1,33 @@
 package model.entities;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Basket {
 	private int id;
 	private float discount;
 	private LocalDate createdAt;
 	private Staff staff;
-	
+	private List<BasketDetail> basketDetails = new ArrayList<BasketDetail>();
+
 	public Basket() {
 		
 	}
 	
-	public Basket(float discount, LocalDate createdAt, Staff staff) {
+	public Basket(float discount, LocalDate createdAt, Staff staff, List<BasketDetail> basketDetails) {
 		this.discount = discount;
 		this.createdAt = createdAt;
 		this.staff = staff;
+		this.basketDetails = basketDetails;
 	}
 	
-	public Basket(int id, float discount, LocalDate createdAt, Staff staff) {
+	public Basket(int id, float discount, LocalDate createdAt, Staff staff, List<BasketDetail> basketDetails) {
 		this.id = id;
 		this.discount = discount;
 		this.createdAt = createdAt;
 		this.staff = staff;
+		this.basketDetails = basketDetails;
 	}
 	
 	public int getId() {
@@ -51,5 +56,13 @@ public class Basket {
 	
 	public void setStaff(Staff staff) {
 		this.staff = staff;
+	}
+	
+	public List<BasketDetail> getBasketDetails() {
+		return basketDetails;
+	}
+
+	public void setBasketDetails(List<BasketDetail> basketDetails) {
+		this.basketDetails = basketDetails;
 	}
 }
