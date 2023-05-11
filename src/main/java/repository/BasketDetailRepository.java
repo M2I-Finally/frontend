8c9a25c638000b55ab38a5e341d78878f75c5c82
Melanie Hautekeur <mehautekeur@gmail.com>
2023-05-11 11:17:40 +0200

    BasketDetailRepository

--- src/main/java/repository/BasketDetailRepository.java
+++ src/main/java/repository/BasketDetailRepository.java
@@ -1,5 +1,29 @@
 package repository;
 
-public class BasketDetailRepository {
+import java.util.ArrayList;
 
+import model.entities.BasketDetail;
+
+public class BasketDetailRepository {
+	
+	public void insert(BasketDetail basketDetail) {
+		//insert the basketDetail in argument in the DB
+	}
+	
+	public ArrayList<BasketDetail> findAll() {
+		//return all the basketDetails in the DB
+		ArrayList<BasketDetail> basketDetailList= new ArrayList<BasketDetail>();
+		return basketDetailList;
+	}
+	public BasketDetail findById(int id) {
+		return new BasketDetail();		
+	}
+	
+	public void update(BasketDetail basketDetail) {
+		//update the basketDetail in argument in the DB
+	}
+	
+	public void delete(int basketDetailId) {
+		//remove the basketDetail with the id in argument from the DB
+	}
 }
