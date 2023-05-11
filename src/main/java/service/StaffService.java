229067627eabc96e159a281ea53d4ad9fda606f2
Huan XIE <kidztse@gmail.com>
2023-05-11 12:13:17 +0200

    add createStaff & getStaffById

--- src/main/java/service/StaffService.java
+++ src/main/java/service/StaffService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class StaffService {
+import model.dto.StaffDto;
+import repository.StaffRepository;
 
+public class StaffService {
+	private StaffRepository repository = new StaffRepository();
+	
+	public void creatStaff(StaffDto staffDto) {
+		//Staff staff = map (staffDto, Staff.class)
+		
+		repository.insert(staff);
+		
+	}
+	
+	
 }
