1f833616e5e3513367c7ee37962e89bf2bb6f80c
Huan XIE <kidztse@gmail.com>
2023-05-11 12:14:44 +0200

    change id type from int to Integer in Staff.java

--- src/main/java/service/StaffService.java
+++ src/main/java/service/StaffService.java
@@ -1,17 +1,24 @@
 package service;
 
 import model.dto.StaffDto;
+import model.entities.Staff;
 import repository.StaffRepository;
 
 public class StaffService {
 	private StaffRepository repository = new StaffRepository();
 	
-	public void creatStaff(StaffDto staffDto) {
+	public void createStaff(StaffDto staffDto) {
 		//Staff staff = map (staffDto, Staff.class)
 		
 		repository.insert(staff);
 		
 	}
 	
-	
+	public Staff getStaffById(Integer staffId ) {
+		if(staffId != null) {
+			return staffRepository.findById(staffId);
+		}
+		
+		throw new Exception("Product ID is null");
+	}
 }
