3817395960f59c9f236030d66a2bd8e347d3e808
MelanieHautekeur <mehautekeur@gmail.com>
2023-06-22 11:33:18 +0200

    Creating genericButton component

---
+++ realFront/src/app/shared/components/generic-button/generic-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-button',
+  templateUrl: './generic-button.component.html',
+  styleUrls: ['./generic-button.component.scss']
+})
+export class GenericButtonComponent {
+
+}
