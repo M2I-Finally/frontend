788e4d362cc000964c3032155f9a454b2cc74220
MelanieHautekeur <mehautekeur@gmail.com>
2023-06-23 09:58:37 +0200

    Creation of yellow button component

---
+++ realFront/src/app/shared/components/yellow-button/yellow-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yellow-button',
+  templateUrl: './yellow-button.component.html',
+  styleUrls: ['./yellow-button.component.scss']
+})
+export class YellowButtonComponent {
+
+}
