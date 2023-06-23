25adfb2ff4320cc36f98a1c42cf739342ed53c35
kt <kidztse@gmail.com>
2023-06-23 12:07:13 +0200

    product card component ok

--- realFront/src/app/shared/shared.module.ts
+++ realFront/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenericButtonComponent } from './components/generic-button/generic-button.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 
 
 @NgModule({
   declarations: [
     GenericButtonComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ProductCardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     GenericButtonComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ProductCardComponent
   ]
 })
 export class SharedModule { }
