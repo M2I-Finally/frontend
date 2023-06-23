61da0297718fbff9be727c043cc5fac905d9f95e
MelanieHautekeur <mehautekeur@gmail.com>
2023-06-23 09:53:34 +0200

    Deleting generic button, creation of red button

---
+++ realFront/src/app/shared/components/red-button/red-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'red-button',
+  templateUrl: './red-button.component.html',
+  styleUrls: ['./red-button.component.scss']
+})
+export class RedButtonComponent {
+
+}
