6ea779b402087dfcaf92a2ef9859a8a6ab9061bd
kt <kidztse@gmail.com>
2023-06-23 16:50:35 +0200

    updated productList for admin-product

--- realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
+++ realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
@@ -14,13 +14,13 @@ import { CommonModule } from '@angular/common';
 export class AdminProductsTableComponent {
 
   productList: Product[] = [
-    {id: 1, name: 'Pain au chocolat', price: 3, isActive: true},
-    {id: 2, name: 'Croissant', price: 3, isActive: true},
-    {id: 3, name: 'Baguette', price: 1.5, isActive: true},
-    {id: 4, name: 'Pain au raison', price: 1, isActive: true},
-    {id: 5, name: 'Chausson au pomme', price: 2, isActive: true},
-    {id: 6, name: 'Brioche', price: 7, isActive: true},
-    {id: 7, name: 'Cookie', price: 2, isActive: true}
+    {id: 1, name: 'Pain au chocolat', price: 3, isActive: true, qty:0},
+    {id: 2, name: 'Croissant', price: 3, isActive: true, qty:0},
+    {id: 3, name: 'Baguette', price: 1.5, isActive: true, qty:0},
+    {id: 4, name: 'Pain au raison', price: 1, isActive: true, qty:0},
+    {id: 5, name: 'Chausson au pomme', price: 2, isActive: true, qty:0},
+    {id: 6, name: 'Brioche', price: 7, isActive: true, qty:0},
+    {id: 7, name: 'Cookie', price: 2, isActive: true, qty:0}
   ];
 
   constructor() {}
