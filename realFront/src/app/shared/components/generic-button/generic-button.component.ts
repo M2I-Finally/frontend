e3572ea63539ff108435b57f71b369158a899c5a
MelanieHautekeur <mehautekeur@gmail.com>
2023-06-23 11:21:14 +0200

    Creation of generic button component, delete all the specific buttons

---
+++ realFront/src/app/shared/components/generic-button/generic-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'generic-button',
+  templateUrl: './generic-button.component.html',
+  styleUrls: ['./generic-button.component.scss']
+})
+export class GenericButtonComponent {
+  @Input() color: string | undefined;
+  @Input() content: string | undefined;
+}
