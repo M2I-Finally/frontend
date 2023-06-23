392035cdb4367a878edb1d55c2cfd021acb4909a
kt <kidztse@gmail.com>
2023-06-23 15:10:38 +0200

    conflit resolved

--- realFront/src/app/app-routing.module.ts
+++ realFront/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { LoginComponent } from './features/user/login/login.component';
 import { GenericHeaderComponent } from './shared/components/generic-header/generic-header.component';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
 
-<<<<<<< HEAD
+
 import { ListRecapComponent } from './features/payment/list-recap/list-recap.component';
-=======
+
 const routes: Routes = [
   {
     path: '',
@@ -16,15 +16,14 @@ const routes: Routes = [
   {
     path: 'button',
     component: GenericButtonComponent,
-  }
->>>>>>> 5511c2b45aef795e20e1bdc4b8d56e765b0de123
-
-const routes: Routes = [
+  },
   {
     path:"list-recap",
     component:ListRecapComponent
   }
-];
+]
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
