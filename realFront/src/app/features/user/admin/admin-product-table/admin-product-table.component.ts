6eb558e5acf4d9f835c3119d245f912dd949c255
kt <kidztse@gmail.com>
2023-06-26 13:15:36 +0200

    mockup datas for admin-product-table connexion stalished

--- realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
+++ realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/entities/product';
 import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CommonModule } from '@angular/common';
+import { ProductService } from 'src/app/mockupData/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'admin-products-table',
@@ -11,25 +13,33 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule, MatTableModule, MatSlideToggleModule],
 })
-export class AdminProductsTableComponent {
+export class AdminProductsTableComponent implements OnInit {
 
-  productList: Product[] = [
-    {id: 1, name: 'Pain au chocolat', price: 3, isActive: true, qty:0},
-    {id: 2, name: 'Croissant', price: 3, isActive: true, qty:0},
-    {id: 3, name: 'Baguette', price: 1.5, isActive: true, qty:0},
-    {id: 4, name: 'Pain au raison', price: 1, isActive: true, qty:0},
-    {id: 5, name: 'Chausson au pomme', price: 2, isActive: true, qty:0},
-    {id: 6, name: 'Brioche', price: 7, isActive: true, qty:0},
-    {id: 7, name: 'Cookie', price: 2, isActive: true, qty:0}
-  ];
+  // productList: Product[] = [
+  //   {id: 1, name: 'Pain au chocolat', price: 3, isActive: true, qty:0},
+  //   {id: 2, name: 'Croissant', price: 3, isActive: true, qty:0},
+  //   {id: 3, name: 'Baguette', price: 1.5, isActive: true, qty:0},
+  //   {id: 4, name: 'Pain au raison', price: 1, isActive: true, qty:0},
+  //   {id: 5, name: 'Chausson au pomme', price: 2, isActive: true, qty:0},
+  //   {id: 6, name: 'Brioche', price: 7, isActive: true, qty:0},
+  //   {id: 7, name: 'Cookie', price: 2, isActive: true, qty:0}
+  // ];
 
-  constructor() {}
+  constructor( private productService: ProductService) {}
+  productList$:Observable<Product[]> | undefined;
+  productList: Product[] = [];
 
+  ngOnInit(): void {
+    this.productList$ = this.productService.getProducts();
+    this.productList$.subscribe(products => { this.productList = products});
+    console.log(this.productList);
+
+  }
   public updateItem(id: number): void {
-    console.log("Redirection to item editing... ID : " + id)
+    console.log("Redirection to item editing... ID : " + this.productList[id].id)
   }
 
   public deleteItem(id: number): void {
-    console.log("Deleted item number " + id);
+    console.log("Deleted item number " + this.productList[id].id);
   }
 }
