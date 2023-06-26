903fba5bc2eae936fb693d03759e589ae4e5a4cc
kt <kidztse@gmail.com>
2023-06-26 13:25:13 +0200

    large logo for login page ok

--- realFront/src/app/features/user/user.module.ts
+++ realFront/src/app/features/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AdminProductsTableComponent } from './admin/admin-product-table/admin-product-table.component';
 
 
 
