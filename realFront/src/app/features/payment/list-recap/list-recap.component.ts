92ad544fa514854802845b11cac8b919a0023193
kt <kidztse@gmail.com>
2023-06-30 10:12:32 +0200

    changed quantity in product mockup in stock, add a temporary cart

--- realFront/src/app/features/payment/list-recap/list-recap.component.ts
+++ realFront/src/app/features/payment/list-recap/list-recap.component.ts
@@ -4,17 +4,6 @@ import { MatTableModule } from '@angular/material/table';
 import { ProductService } from 'src/app/mockupData/product.service';
 import { Product } from 'src/app/shared/entities/product';
 
-//mockup data
-// let productList: Product[] = [
-//   {id: 1, name: 'Pain au chocolat', price: 3, isActive: true, qty:0},
-//   {id: 2, name: 'Croissant', price: 3, isActive: true, qty:0},
-//   {id: 3, name: 'Baguette', price: 1.5, isActive: true, qty:0},
-//   {id: 4, name: 'Pain au raison', price: 1, isActive: true, qty:0},
-//   {id: 5, name: 'Chausson au pomme', price: 2, isActive: true, qty:0},
-//   {id: 6, name: 'Brioche', price: 7, isActive: true, qty:0},
-//   {id: 7, name: 'Cookie', price: 2, isActive: true, qty:0}
-// ];
-
 @Component({
   selector: 'app-list-recap',
   templateUrl: './list-recap.component.html',
@@ -37,14 +26,14 @@ export class ListRecapComponent implements OnInit{
   }
 
   minus(id: number){
-    if (this.productList[id].qty > 0) {
-      this.productList[id].qty--;
+    if (this.productList[id].quantity > 0) {
+      this.productList[id].quantity--;
       //function to add with cart
     }
   }
 
   add(id: number){
-    this.productList[id].qty++;
+    this.productList[id].quantity++;
     //function to add with cart
   }
 }
