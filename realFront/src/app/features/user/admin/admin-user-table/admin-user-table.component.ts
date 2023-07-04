0893a188a44dd0b46c7848bf2085b61bb1526e2b
MelanieHautekeur <mehautekeur@gmail.com>
2023-07-04 16:53:53 +0200

    Creation of user form

--- realFront/src/app/features/user/admin/admin-user-table/admin-user-table.component.ts
+++ realFront/src/app/features/user/admin/admin-user-table/admin-user-table.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/mockupData/user.service';
 
 @Component({
   selector: 'app-admin-user-table',
   templateUrl: './admin-user-table.component.html',
   styleUrls: ['./admin-user-table.component.scss']
 })
-export class AdminUserTableComponent {
+export class AdminUserTableComponent implements OnInit {
+  modeText: string = "Ajouter";
 
+  constructor(private router: Router, private userService: UserService) {};
+
+  formUser = new UntypedFormGroup({
+    userId: new UntypedFormControl(''),
+    userName: new UntypedFormControl(''),
+    userPassword: new UntypedFormControl(''),
+    confirmationPassword: new UntypedFormControl(''),
+    userRole: new UntypedFormControl(''),
+  })
+
+  ngOnInit(): void {
+    
+  }
+
+  goToPage(pageName:string): void {
+    this.router.navigate([`${pageName}`])
+  }
 }
