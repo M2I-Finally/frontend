5badb861edd6523d9982f2e4906bc0e14fe9bd6c
MelanieHautekeur <mehautekeur@gmail.com>
2023-07-04 12:30:09 +0200

    Adding current date to the logout page

--- realFront/src/app/features/user/logout/logout.component.ts
+++ realFront/src/app/features/user/logout/logout.component.ts
@@ -1,10 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.scss']
+  styleUrls: ['./logout.component.scss'],
+  providers: [DatePipe]
 })
 export class LogoutComponent {
-
+  currentDate: Date = new Date();
 }
