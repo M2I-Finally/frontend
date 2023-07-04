0664c744189a1df7caabff0a25e9fdcaf945330a
VAUBAN_C3\Administrateur <sebd.dev@gmail.com>
2023-07-04 09:32:16 +0200

    implementation of business rules for basket management

--- realFront/src/app/shared/components/basket/basket.component.ts
+++ realFront/src/app/shared/components/basket/basket.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component,Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Basket } from '../../entities/basket';
+import { CartLine } from '../../entities/cart-line';
+import { Observable, map ,of} from 'rxjs';
+import { Cart } from '../../entities/cart';
+import { BasketService } from '../../services/basket.service';
 
 @Component({
   selector: 'basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.scss']
 })
-export class BasketComponent {
+export class BasketComponent implements OnInit {
 
+  constructor(private basketService : BasketService){}
+  //basket : Basket = new Basket("panier");
+  //basket$: Observable<CartLine[]> | undefined;
+  @Input() quantity!: number;
+  total!: number; 
+  basket$!: Cart;
+  cartLine$! : CartLine[];
+  
+  
+ ngOnInit(): void {
+  this.basketService.basket$.subscribe((basket: Cart) => {
+    this.basket$ = basket;
+    this.cartLine$ = basket.getCartLines()
+    this.calculateTotal2();
+  });
+ }
+ calculateTotal(): void {
+  // Effectuez le calcul du total en utilisant les données de cartLine$
+  this.total = this.cartLine$.reduce(
+    (acc, cartLine) => acc + cartLine.getPrice() * cartLine.getQuantity(),
+    0
+  );
+}
+
+calculateTotal2(): void {
+  let total = 0;
+
+  this.cartLine$.forEach((cartLine) => {
+    total += cartLine.getPrice() * cartLine.getQuantity() * cartLine.getDiscount();
+  });
+
+  this.total = total;
+}
+ 
+
+  
 }
