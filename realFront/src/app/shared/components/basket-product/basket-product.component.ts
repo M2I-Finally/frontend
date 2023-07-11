6a5553aba227fd05700161adad4529066a56bd8b
VAUBAN_C3\Administrateur <sebd.dev@gmail.com>
2023-07-11 17:16:39 +0200

    add method and initiaton for quantity : shop -> productCard -> basketCard

--- realFront/src/app/shared/components/basket-product/basket-product.component.ts
+++ realFront/src/app/shared/components/basket-product/basket-product.component.ts
@@ -1,5 +1,8 @@
 import { Component,Input } from '@angular/core';
 import { CartLine } from '../../entities/cart-line';
+import { BasketService } from '../../services/basket.service';
+import { Cart } from '../../entities/cart';
+import { Basket } from '../../entities/basket';
 
 @Component({
   selector: 'basket-product',
@@ -7,21 +10,59 @@ import { CartLine } from '../../entities/cart-line';
   styleUrls: ['./basket-product.component.scss']
 })
 export class BasketProductComponent {
-    @Input() cartLine? :CartLine ;
-   
+  constructor(private basketService : BasketService){}
+  basket$!: Cart;
+  @Input() cartLine? :CartLine ;
+  totalLine : number = 0;
+  
+  ngOnInit(): void {
+    this.basketService.basket$.subscribe((basket: Cart) => {
+      this.basket$ = basket;
+      if(this.cartLine)
+     this.totalLine = this.cartLine?.getQuantity() * this.cartLine.getPrice();
+    });
+   }
+ 
+  minus(){
     
-    minus(){
-     
-      if(this.cartLine && this.cartLine?.getQuantity() > 0)
-       this.cartLine.setQuantity(-1);     
+    if(this.cartLine && this.cartLine?.getQuantity() > 0){
+      this.basket$.getCartLines().forEach((line) => {
+        if( this.cartLine?.getId() == line.getId() && line.getQuantity() > 0){           
+          line.setQuantity(-1);         
+         
+          if (line.getQuantity()<=0){   
+            this.basket$.removeLines(this.cartLine.getId());               
+          }            
+      }
+      this.basketService.updateBasket(this.basket$);
+    })
+    }     
     
   }
 
   add(){
-    
-    if(this.cartLine)
-       this.cartLine.setQuantity(1);
+   
+       if (this.cartLine){
+          this.basket$.getCartLines().forEach((line) => {
+  
+          if( this.cartLine?.getId() == line.getId()){ 
+            
+              line.setQuantity(1);      
+                  
+          }
+  
+        })
+              
+        this.basketService.updateBasket(this.basket$);
+      }               
   }
 
+  trash(){
+    if(this.cartLine){
+      this.basket$.removeLines(this.cartLine.getId());
+      this.basketService.updateBasket(this.basket$);
+    }
+      
+  }
     
 }
