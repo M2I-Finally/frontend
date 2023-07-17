f8d13161da411cac2fb7c30d845ad3346449f123
Etienne Verraest <verraest.etienne@gmail.com>
2023-07-17 13:18:00 +0200

    Added modal on delete product

--- realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
+++ realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
@@ -12,20 +12,35 @@ export class AdminProductsTableComponent implements OnInit {
 
   constructor(private productService: ProductService) {}
   
-  productList$:Observable<Product[]> | undefined;
+  modalDelete!: HTMLDialogElement;
+  protected productList$:Observable<Product[]> | undefined;
+  selectedProduct: Product | undefined;
 
   public ngOnInit(): void {
+
+    this.modalDelete = document.getElementById("delete-dialog") as HTMLDialogElement;
+
     // This will merge the new results even after page refresh
     this.productList$ =  this.productService.getProducts().pipe(
       mergeMap(() => this.productService.getProducts())
     );
   }
 
-  protected deleteItem(tableIndex: number, productId: number): void {
+  protected showDeleteModal(product: Product): void {
+    this.selectedProduct = product;
+    this.modalDelete.showModal();
+  }
+
+  protected closeDeleteModal(): void {
+    this.modalDelete.close();
+  }
+  
+  protected deleteProduct(productId: number): void {
     // This will subscribe to the only result returned from the observable (and thus unsubscribe)
     this.productList$ = this.productService.deleteProduct(productId).pipe(
       mergeMap(() => this.productService.getProducts())
     );
+    
+    this.closeDeleteModal();
   }
-
 }
