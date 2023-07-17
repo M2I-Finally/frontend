6e88cc40cea91bb92a35eea920fb022922e28d35
Etienne Verraest <verraest.etienne@gmail.com>
2023-07-17 14:47:20 +0200

    Modified toggle button on admin product table

--- realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
+++ realFront/src/app/features/user/admin/admin-product-table/admin-product-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/entities/product';
 import { ProductService } from 'src/app/mockupData/product.service';
-import { Observable, mergeMap,} from 'rxjs';
+import { Observable, first, mergeMap} from 'rxjs';
 
 @Component({
   selector: 'admin-products-table',
@@ -12,9 +12,9 @@ export class AdminProductsTableComponent implements OnInit {
 
   constructor(private productService: ProductService) {}
   
-  modalDelete!: HTMLDialogElement;
+  protected modalDelete!: HTMLDialogElement;
   protected productList$:Observable<Product[]> | undefined;
-  selectedProduct: Product | undefined;
+  protected selectedProduct: Product | undefined;
 
   public ngOnInit(): void {
 
@@ -26,18 +26,35 @@ export class AdminProductsTableComponent implements OnInit {
     );
   }
 
+  // Change active state of a product when clicked
+  protected changeActiveState(event: Event, productId: number): void {
+   
+    const checkbox = event.target as HTMLInputElement;
+    console.log(checkbox);
+
+    if(checkbox.checked == true) {
+      console.log("Handling active state");
+    } else {
+      console.log("Handling inactive state")
+    }
+
+  }
+
+  // Shows the delete modal with appropriate product settings
   protected showDeleteModal(product: Product): void {
     this.selectedProduct = product;
     this.modalDelete.showModal();
   }
 
+  // Closes the delete modal
   protected closeDeleteModal(): void {
     this.modalDelete.close();
   }
   
   protected deleteProduct(productId: number): void {
+    
     // This will subscribe to the only result returned from the observable (and thus unsubscribe)
-    this.productList$ = this.productService.deleteProduct(productId).pipe(
+    this.productList$ = this.productService.deleteProduct(productId).pipe(first(),
       mergeMap(() => this.productService.getProducts())
     );
     
