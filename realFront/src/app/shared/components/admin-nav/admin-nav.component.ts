8168e26361b12c0ad7f5fb3142c1bf4ce2918ad9
MelanieHautekeur <mehautekeur@gmail.com>
2023-07-18 09:18:58 +0200

    Creating routes on admin nav button

--- realFront/src/app/shared/components/admin-nav/admin-nav.component.ts
+++ realFront/src/app/shared/components/admin-nav/admin-nav.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'admin-nav',
   templateUrl: './admin-nav.component.html',
   styleUrls: ['./admin-nav.component.scss']
 })
-export class AdminNavComponent {
+export class AdminNavComponent {  
+  constructor(private router: Router) {};
 
+  goToPage(pageName:string): void {
+    this.router.navigate([`${pageName}`])
+  }
 }
