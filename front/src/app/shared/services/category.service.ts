8ffdda3a6fb0d1f678748c4eec5ea4d9159bb7e3
etienne-verraest <verraest.etienne@gmail.com>
2023-07-29 20:19:57 +0200

    [Config] Added environment file with api url

--- front/src/app/shared/services/category.service.ts
+++ front/src/app/shared/services/category.service.ts
@@ -3,33 +3,33 @@ import { Injectable } from '@angular/core';
 import { Category } from '../entities/category';
 import { Observable } from 'rxjs';
 import { CreateCategory } from '../entities/create-category';
+import { Environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  url = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.url + '/categories/all');
+    return this.http.get<Category[]>(Environment.apiUrl + '/categories/all');
   }
 
   postCategory(categoryToCreate: CreateCategory): Observable<Category> {
-    return this.http.post<Category>(this.url + '/categories', categoryToCreate);
+    return this.http.post<Category>(Environment.apiUrl + '/categories', categoryToCreate);
   }
 
   deleteCategory(categoryId: number): Observable<Category> {
-    return this.http.delete<Category>(this.url + '/categories/' + categoryId);
+    return this.http.delete<Category>(Environment.apiUrl + '/categories/' + categoryId);
   }
 
   patchCategoryStatus(categoryId: number): Observable<Category> {
-    return this.http.patch<Category>(this.url + '/categories/status/' + categoryId, {});
+    return this.http.patch<Category>(Environment.apiUrl + '/categories/status/' + categoryId, {});
   }
 
   patchCategoryName(categoryId: number, nameToSet: string): Observable<Category> {
-    return this.http.patch<Category>(this.url + '/categories/name/' + categoryId, {
+    return this.http.patch<Category>(Environment.apiUrl + '/categories/name/' + categoryId, {
       name: nameToSet
     });
   }
