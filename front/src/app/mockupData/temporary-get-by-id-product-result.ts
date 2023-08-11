f80ecbaa3a91334db82d6840fd0b94c7a3e9aa98
etienne-verraest <verraest.etienne@gmail.com>
2023-08-11 15:06:01 +0200

    Removed mockupData folder

--- front/src/app/mockupData/temporary-get-by-id-product-result.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "../shared/entities/product";
-
-// This interface is temporary, until we get a functioning back-end (we won't need length metadata)
-export interface TemporaryGetByIdProductResult {
-    0: Product;
-    length: number;
-}
