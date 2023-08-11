da7dc8b1b800d75d2997ee604dcb4b5c71f64dae
etienne-verraest <verraest.etienne@gmail.com>
2023-08-11 15:41:15 +0200

    401 errors are now handled in login

--- front/src/app/features/user/login/login.component.ts
+++ front/src/app/features/user/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Jwt } from 'src/app/shared/entities/jwt';
+import jwt_decode from "jwt-decode";
 import { Auth } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -9,40 +11,43 @@ import { Auth } from 'src/app/shared/services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  content = "connexion";
-  color = "yellow";
+export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private authService : Auth,private toastr: ToastrService) {}
-  
   loginForm : UntypedFormGroup= new UntypedFormGroup({
     username: new UntypedFormControl('admin',[Validators.required]),
     password: new UntypedFormControl('admin',[Validators.required])
   })
-
-  submit(){
-   this.authService.login(this.loginForm.get('username')?.value,this.loginForm.get('password')?.value).subscribe({
-    next: (loggedIn: boolean) => {
-      if (loggedIn) {
-        console.log("redirection vers shop")
-        this.router.navigateByUrl('/shop')
-      } else {
-        console.log(loggedIn + " login non valide")
-      }
-    },
-    error: (error) => {
-      if (error.status === 400) {
-        console.log("Erreur 400: Identifiants invalides");        
-      } else {
-        console.log("Erreur inattendue: " + error.error.message);
-        this.toastr.error(error.message)        
-      }
-      this.toastr.error(error.error.message)
+  
+  constructor(private router: Router, private authService : Auth,private toastr: ToastrService) {}
+  
+  // If user is already logged in we redirect him to the shop page
+  ngOnInit(): void {
+    const token = sessionStorage.getItem('token');
+    
+    let jwtDecoced : Jwt = jwt_decode(token!);
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+  
+    if(token && jwtDecoced.exp >= currentTimestamp){
+      this.router.navigateByUrl('/shop')
     }
-  });
   }
-
-  goToPage(pageName:string): void {
-    this.router.navigate([`${pageName}`])
-  }
+  
+  submit() {
+    this.authService.login(this.loginForm.get('username')?.value,this.loginForm.get('password')?.value).subscribe({
+      next: (loggedIn: boolean) => {
+        if (loggedIn) {
+          this.router.navigateByUrl('/shop')
+        }
+      },
+      error: (error) => {
+          if (error.status === 403) {
+            this.toastr.error("Les identifiants sont invalides");  
+          } else if(error.status === 0 ){
+            this.toastr.info("Le serveur ne semble pas être démarré");  
+          }
+          else {   
+            this.toastr.error(error.error.message);
+          }
+        }});
+    }
 }
--- front/src/app/shared/services/interceptor.service.ts
+++ front/src/app/shared/services/interceptor.service.ts
@@ -4,46 +4,41 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { Jwt } from '../entities/jwt';
 import jwt_decode from "jwt-decode";
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor{
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private toastr: ToastrService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = sessionStorage.getItem('token');
     
-    let jwtDecoced : Jwt = jwt_decode(token!);
+    let jwtDecoded : Jwt = jwt_decode(token!);
     const currentTimestamp = Math.floor(Date.now() / 1000);
   
-    if(token && jwtDecoced.exp >= currentTimestamp){
+    if(token && jwtDecoded.exp >= currentTimestamp){
       const httpOptions = {
         headers: new HttpHeaders({
-          //"Access-Control-Allow-Origin" : "*",
-          //"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
-          "authorization" : `Bearer ${token}`      
+          "Authorization" : `Bearer ${token}`      
         })
       };
-      
       req = req.clone(httpOptions)      
-    }else {
+
+    } else {
       sessionStorage.clear();
       this.router.navigate(['/']);
     }
 
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401 || error.status === 403) {
-         console.log("OMG une erreur 401 ou 403")
+        if (error.status === 0) {
+          this.router.navigate(['/']);       
         }
-       
-        return throwError(() => new Error("dans le throws : "+error.message));
+        return throwError(() => new Error("Unexpected error : "+error.message));
       })
     );
-
-    
-   
   }
 }
