02f3739f1308c4a768a5e9093d68a9353f2af0f5
MelanieHautekeur <mehautekeur@gmail.com>
2023-08-16 16:15:55 +0200

    Not displaying user button when the connected user role is not admin - Need to do the same for the mobile version of the nav

--- front/src/app/shared/components/admin-nav/admin-nav.component.ts
+++ front/src/app/shared/components/admin-nav/admin-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import jwt_decode from "jwt-decode";
+import { Jwt } from 'src/app/shared/entities/jwt';
 
 @Component({
   selector: 'admin-nav',
@@ -8,6 +10,19 @@ import { Router } from '@angular/router';
 })
 export class AdminNavComponent {  
   constructor(private router: Router) {};
+
+  // By default we assume that it is a manager, so we don't show user button by error
+  protected isAdmin: boolean = false;
+
+  ngOnInit(): void {
+    let sessionToken = sessionStorage.getItem('token');
+    if(sessionToken != undefined) {
+      let decoded: Jwt = jwt_decode(sessionToken);
+      if( decoded.role == 'ADMIN' ) {
+        this.isAdmin = true;
+      }
+    }
+  }
 
   goToPage(pageName:string): void {
     this.router.navigate([`${pageName}`])
