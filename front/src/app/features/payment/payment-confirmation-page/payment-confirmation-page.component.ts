3dc13c83a164fdd686de5ce1a92d2e02df739513
HX <kidztse@gmail.com>
2023-08-16 15:54:02 +0200

    payment confirmation features ok

--- front/src/app/features/payment/payment-confirmation-page/payment-confirmation-page.component.ts
+++ front/src/app/features/payment/payment-confirmation-page/payment-confirmation-page.component.ts
@@ -22,37 +22,35 @@ import { _isNumberValue } from '@angular/cdk/coercion';
 export class PaymentConfirmationPageComponent implements OnInit {
   total!: number
   totalAfterDiscount: number | undefined;
-  cartModified:boolean | undefined;
+  cartModified: boolean | undefined;
   basket$!: Cart;
-  basketLine!:CartLine[];
-  amount:number = 0;
-  amountDue:number = 0;
-  amountPaid:number = 0;
-  change:number= 0;
-  paymentTypeId:number | undefined;
+  basketLine!: CartLine[];
+  amount: number = 0;
+  amountDue: number = 0;
+  amountPaid: number = 0;
+  change: number = 0;
+  paymentTypeId: number | undefined;
   payment$!: Payment;
-  sellerId:number = 999;
-  discount:number = 1;
+  sellerId: number = 999;
+  discount: number = 1;
   paymentDtoList: PaymentDto[] = [];
-  idPaidBAsket : number = 0;
- 
+  idPaidBAsket: number = 0;
 
+  constructor(private basketService: BasketService, private paymentService: PaymentService, private toastr: ToastrService, private router: Router) { };
 
-  constructor(private basketService: BasketService, private paymentService:PaymentService,private toastr: ToastrService, private router: Router) { };
-  
   formPayByCard = new UntypedFormGroup({
-    sumCard:new UntypedFormControl('', [Validators.required, Validators.min(0), Validators.max(this.total)]),
-    BANK_CAR:new UntypedFormControl(1)
+    sumCard: new UntypedFormControl(''),
+    BANK_CAR: new UntypedFormControl(1)
   });
-  
+
   formPayByCash = new UntypedFormGroup({
-    sumCash:new UntypedFormControl('', [Validators.required, Validators.min(0), Validators.max(this.total)]),
-    CASH:new UntypedFormControl(0)
+    sumCash: new UntypedFormControl(''),
+    CASH: new UntypedFormControl(0)
   });
 
   formPayByOther = new UntypedFormGroup({
-    sumOther:new UntypedFormControl('', [Validators.required, Validators.min(0),Validators.max(this.total)]),
-    OTHER:new UntypedFormControl(2)
+    sumOther: new UntypedFormControl(''),
+    OTHER: new UntypedFormControl(2)
   });
 
   ngOnInit(): void {
@@ -64,7 +62,7 @@ export class PaymentConfirmationPageComponent implements OnInit {
       this.total = this.basket$.getTotal();
       this.basketLine = basket.getCartLines();
     })
-    
+
     /**
      * initialize amountDue
      */
@@ -76,23 +74,23 @@ export class PaymentConfirmationPageComponent implements OnInit {
     let sessionToken = sessionStorage.getItem('token');
     let decoded: Jwt = jwt_decode(sessionToken!);
     this.sellerId = decoded.id;
-    
+
   };
 
   /**
    * update total if there is any discount change
    * @param event totalAfterDiscount from recap-list
    */
-  totalWithDiscount(event:number){
+  totalWithDiscount(event: number) {
     this.totalAfterDiscount = event;
     this.amountDue = this.totalAfterDiscount;
     // if totalAfterDiscount is smaller than total, means we have applied a discount and the total should be updated. 
-    if (this.totalAfterDiscount < this.total){     
-      this.discount = 1 - this.totalAfterDiscount/this.total;
+    if (this.totalAfterDiscount < this.total) {
+      this.discount = 1 - this.totalAfterDiscount / this.total;
       this.total = this.totalAfterDiscount;
-      this.basket$.setTotal(this.total);     
+      this.basket$.setTotal(this.total);
       this.isCartLineModified(true);
-    } 
+    }
   };
 
   /**
@@ -103,7 +101,7 @@ export class PaymentConfirmationPageComponent implements OnInit {
    * @param name FormControlName for input
    * @param paymentType 0 = cash, 1 = bank card, 2 = other
    */
-  updatePayment(formGroupName:UntypedFormGroup, name:string, paymentType:string){
+  updatePayment(formGroupName: UntypedFormGroup, name: string, paymentType: string) {
     // get the payment input
     this.amount = formGroupName.controls[name].value;
 
@@ -111,61 +109,61 @@ export class PaymentConfirmationPageComponent implements OnInit {
     this.paymentTypeId = parseInt(formGroupName.controls[paymentType].value);
 
     // update total
-    if (this.total>0){
+    if (this.total > 0) {
       this.amountPaid += this.amount;
-      if(this.amountPaid<this.total){
-        this.amountDue = this.total - this.amountPaid;  
+      if (this.amountPaid < this.total) {
+        this.amountDue = this.total - this.amountPaid;
       } else {
         this.amountDue = 0;
-
-        if(this.paymentTypeId===2){
-          this.change=0;
-        } else{
+        // no changes need when payment on chèque de vanceces or chèque de restaurant
+        if (this.paymentTypeId === 2) {
+          this.change = 0;
+        } else {
           this.change = this.amountPaid - this.total;
         }
       }
-      
+
     } else {
       this.toastr.error("Il n'y a rien à payer.")
     }
 
     // note partial payment amount and id, if there is change, we need to note (cash received - change) to bdd
-    if (this.paymentTypeId === 0 && this.change){
-      this.paymentDtoList.push(new PaymentDto((this.amount-this.change), this.paymentTypeId));
+    if (this.paymentTypeId === 0 && this.change) {
+      this.paymentDtoList.push(new PaymentDto((this.amount - this.change), this.paymentTypeId));
     } else {
       this.paymentDtoList.push(new PaymentDto(this.amount, this.paymentTypeId));
     }
-    
+
   }
 
   /**
     * total payment
     * @param paymentTypeId 0 = cash, 1 = bank card, 2 = other
     */
-  Submit(paymentTypeId:number){
-    this.paymentDtoList.push(new PaymentDto(this.amountDue,paymentTypeId));
+  Submit(paymentTypeId: number) {
+    this.paymentDtoList.push(new PaymentDto(this.amountDue, paymentTypeId));
     this.amountDue = 0;
-    this.createBasket();    
+    this.createBasket();
   }
-  
-   /**
-   * Any modification for quantity will update payment situation.
-   * @param event if quantity is changed. 
-   */
-   isCartLineModified(event:boolean){
-    this.cartModified = event;
-    
-    if (this.cartModified){
-      
-      this.amountDue = this.total-this.amountPaid;
-      if(this.amountDue>=0){
-        this.change = 0;
-      } 
 
-      if(this.amountDue<0) {
+  /**
+  * Any modification for quantity will update payment situation.
+  * @param event if quantity is changed. 
+  */
+  isCartLineModified(event: boolean) {
+    this.cartModified = event;
+
+    if (this.cartModified) {
+
+      this.amountDue = this.total - this.amountPaid;
+      if (this.amountDue >= 0) {
+        this.change = 0;
+      }
+
+      if (this.amountDue < 0) {
         this.change = this.amountPaid - this.total
         this.amountDue = 0
-      } 
+      }
     }
   }
 
@@ -174,44 +172,44 @@ export class PaymentConfirmationPageComponent implements OnInit {
    * reset the cart and noted payment list
    * @param page back to shop
    */
-  protected cancelBasket(page:string): void {
+  protected cancelBasket(page: string): void {
     this.basket$.resetCart();
     this.clearPaymentList();
-    this.router.navigate([page]);  
+    this.router.navigate([page]);
   }
 
   /**
    * valid payment, get success or fail message
    * payment will not pass when there is nothing in the cart nor the cart is been paid.
    */
-  protected createBasket():void{
+  protected createBasket(): void {
     // cart cannot be empty
-    if (this.total > 0 ){
-      
+    if (this.total > 0) {
+
       //payment needs to be done
       if (this.amountDue > 0) {
         this.toastr.error("Veuillez finir le paiement.");
       } else {
         this.paymentService.postPayment(
           new Payment(this.basketLine,
-             this.total,
-             this.discount,
-             this.paymentDtoList,
-             this.sellerId)
-         ).subscribe({
-          next: (data) =>{
+            this.total,
+            this.discount,
+            this.paymentDtoList,
+            this.sellerId)
+        ).subscribe({
+          next: (data) => {
             this.toastr.success(`Le panier ${data} est bien enregistré, la facture est encours de généreration.`);
             //sauvegarde du panier payer dans le basket service pour edition de la facture
-            let paidBasket = new Cart(this.basket$.getCartLines(),this.basket$.getTotal(),this.discount)
-            this.idPaidBAsket = data;                       
-            this.basketService.SavePaidBasket(paidBasket,data,this.paymentDtoList); 
+            let paidBasket = new Cart(this.basket$.getCartLines(), this.basket$.getTotal(), this.discount)
+            this.idPaidBAsket = data;
+            this.basketService.SavePaidBasket(paidBasket, data, this.paymentDtoList);
             // une fois payé, vider le panier et avancer sur la page facture 
             this.cancelBasket('facture');
-          }, 
+          },
           error: error => this.toastr.error(error.message)
-         });
+        });
       }
-   
+
     } else {
       this.toastr.error("Le panier n'est pas encore crée.");
       this.cancelBasket('shop');
@@ -221,9 +219,9 @@ export class PaymentConfirmationPageComponent implements OnInit {
   /**
    * clear noted payment amount and id in paymentList
    */
-  private clearPaymentList(){
-    if (this.paymentDtoList.length > 0){     
-      for (let i=0; i<this.paymentDtoList.length; i++){
+  private clearPaymentList() {
+    if (this.paymentDtoList.length > 0) {
+      for (let i = 0; i < this.paymentDtoList.length; i++) {
         this.paymentDtoList.pop();
       }
     };
